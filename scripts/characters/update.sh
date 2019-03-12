9356f34a28e6d00ba742bfdee740ebb15837a8d4
smd9788 <smd09876@gmail.com>
2019-03-12 14:45:37 -0400

    fixed update.sh curl script

--- scripts/characters/update.sh
+++ scripts/characters/update.sh
@@ -7,12 +7,12 @@ curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
---header "Authorization: Bearer ${TOKEN}" \
---data '{
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
     "character": {
-      "nickname": "'"${NICKNAME}"'",  
+      "nickname": "'"${NICKNAME}"'",
       "level": "'"${LEVEL}"'",
-      "charClass": "'"${CLASS}"'",
+      "charClass": "'"${CHARCLASS}"'"
     }
   }'
 
