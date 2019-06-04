2791d6fefebf5186d89a9077feb123d77be132af
smd9788 <smd09876@gmail.com>
2019-06-04 11:38:46 -0400

    Updated repo name. Updated config and packages for new name

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'battlegame-api'
+const mongooseBaseName = 'herobuilder-api'
 
 // create the mongodb uri for development and test
 const database = {
